9578bd7abc63f4cd7ca54dcac29512f2c0ba6295
Ivan Gorshkov <ivanagorshkov160@gmail.com>
2020-03-08 12:22:09 +0300

    fix a bit CodeStyle

--- Makefile
+++ Makefile
@@ -1,12 +1,11 @@
 TARGET = main.out
-HDRS_DIR = project/include
-
+HDRS_DIR = project/include/
 SRCS = project/src/main.c
 
 .PHONY: all clean
 
 all: $(SRCS)
-	$(CC) -Wall -Wextra -Werror -o $(TARGET) $(CFLAGS) $(SRCS)
+	$(CC) -Wall -Wextra -Werror -I $(HDRS_DIR) -o $(TARGET) $(CFLAGS) $(SRCS)
 
 clean:
 	rm -rf $(TARGET)
--- project/include/utils.h
+++ project/include/utils.h
@@ -1,9 +1,12 @@
 #define UTILS_H
 #ifndef UTILS_H
 
-#define filename "transaction.dat";
+#include <stdio.h>
 
+#define filename "transaction.dat";
 // This comment is very useful
 printf("This message is very useful too")
-
+void masterWrite(FILE *ofPTR, Data Client);
+void transactionWrite(FILE *ofPTR, Data transfer);
+void blackRecord(FILE *ofPTR, FILE *ofPTR_2, FILE *blackrecord, Date client_data, Data transfer);
 #endif //UTILS_H
