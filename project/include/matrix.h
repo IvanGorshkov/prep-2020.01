9f25e63ec07ce29e5b4176665bff68f5b6f1769d
Ivan Gorshkov <ivanagorshkov160@gmail.com>
2020-03-17 12:34:56 +0300

    creat struct, creat_matrix, get rows and cols and free matrix

--- project/include/matrix.h
+++ project/include/matrix.h
@@ -5,6 +5,9 @@
 
 
 typedef struct Matrix {
+    size_t rows;
+    size_t cols;
+    double** matrix;
 } Matrix;
 
 // Init/release operations
--- project/src/matrix.c
+++ project/src/matrix.c
@@ -1,4 +1,57 @@
 #include "matrix.h"
 
-// NOTE(stitaevskiy): Place your implementation here
 
+Matrix* create_matrix(size_t rows, size_t cols) {
+    if(rows = 0 || cols = 0) {
+        return NULL;
+    }
+
+    Matrix* matrix = calloc(1, sizeof(Matrix));
+
+    if(matrix == NULL) {
+        printf("%s","Can not calloc memory");
+        return NULL[;]
+    }
+
+    matrix->rows = rows;
+    matrix->cols = cols;
+    matrix->matrix = calloc(rows*cols, sizeof(double));
+
+    if(matrix->matrix == NULL) {
+        printf("%s","Can not calloc memory");
+        return NULL;
+    }
+    return matrix;
+}
+
+int free_matrix(Matrix* matrix) {
+    if (matrix == NULL) {
+        return EXIT_FAILURE;
+    }
+
+    free(matrix->matrix);
+    free(matrix);
+
+    if (matrix == NULL && matrix->matrix == NULL) {
+        return EXIT_SUCCESS;
+    }
+    return EXIT_FAILURE;
+}
+
+int get_rows(const Matrix* matrix, size_t* rows) {
+    if (matrix == NULL && rows == NULL) {
+        return EXIT_FAILURE;
+    }
+
+    *rows = matrix->rows;
+    return EXIT_SUCCESS;
+}
+
+int get_cols(const Matrix* matrix, size_t* cols) {
+    if (matrix == NULL && cols == NULL) {
+        return EXIT_FAILURE;
+    }
+
+    *cols = matrix->cols;
+    return EXIT_SUCCESS;
+}
