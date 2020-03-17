1a5ff268edb76bd05c947d80d42738a9d1694084
Ivan Gorshkov <ivanagorshkov160@gmail.com>
2020-03-17 18:03:06 +0300

    add mul

--- project/src/main.c
+++ project/src/main.c
@@ -17,21 +17,24 @@ int main(void) {
     set_elem(matrix, 3, 3, 9);
      printf("\n");
     set_elem(matrix, 4, 1, 10);
-    set_elem(matrix, 4, 2, 12);
-    set_elem(matrix, 4, 3, 13);
+    set_elem(matrix, 4, 2, 11);
+    set_elem(matrix, 4, 3, 12);
     
     double val;
     
-    Matrix *mul = mul_scalar(matrix,2);
-    get_elem(mul, 4, 3, &val);
-    printf("%f\n", val);
-    
-    Matrix *summatrix = sum(mul, matrix);
+    Matrix *muls = mul_scalar(matrix,2);
+    get_elem(muls, 4, 3, &val);
+   // printf("%f\n", val);
+    Matrix *trans = transp(matrix);
+    Matrix *summatrix = sum(muls, matrix);
     get_elem(summatrix, 4, 3, &val);
-       printf("%f\n", val);
+     //  printf("%f\n", val);
     Matrix *submatrix = sub(summatrix, matrix);
     get_elem(submatrix, 4, 3, &val);
-       printf("%f\n", val);
+     //  printf("%f\n", val);
+    Matrix *mul_matrix = mul(matrix, trans);
+    get_elem(mul_matrix, 2, 2, &val);
+   // printf("%f\n", val);
     return 0;
 }
 
--- project/src/matrix.c
+++ project/src/matrix.c
@@ -73,10 +73,11 @@ int set_elem(Matrix* matrix, size_t row, size_t col, double val) {
     if (matrix == NULL || row <= 0 || col <= 0) {
         return EXIT_FAILURE;
     }
-    
+
     if (matrix->cols < col || matrix->rows < row) {
         return EXIT_FAILURE;
     }
+
     matrix->matrix[(matrix->cols * (row-1))+col-1] = val;
     return EXIT_SUCCESS;
 }
@@ -85,16 +86,16 @@ Matrix* mul_scalar(const Matrix* matrix, double val) {
     if(matrix == NULL) {
         return NULL;
     }
-    
+
     Matrix* mul_matrix = create_matrix(matrix->rows, matrix->cols);
-    
+
     if(mul_matrix == NULL) {
         return NULL;
     }
-    
+
     int cols = mul_matrix->cols;
     int rows = mul_matrix->rows;
-    
+
     for (int i = 0; i < cols*rows; i++) {
             double data = matrix->matrix[i] * val;
             mul_matrix->matrix[i] = data;
@@ -106,16 +107,16 @@ Matrix* transp(const Matrix* matrix) {
     if(matrix == NULL) {
         return NULL;
     }
-    
+
     Matrix* transp_matrix = create_matrix(matrix->cols, matrix->rows);
-    
+
     if(transp_matrix == NULL) {
         return NULL;
     }
-    
+
     int cols = transp_matrix->cols;
     int rows = transp_matrix->rows;
-    printf("%i\n",cols);
+
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             transp_matrix->matrix[transp_matrix->cols * i+j] = matrix->matrix[matrix->cols * j+i];
@@ -128,55 +129,121 @@ Matrix* sum(const Matrix* l, const Matrix* r) {
     if(l == NULL || r == NULL) {
         return NULL;
     }
-    
+
     if(l->cols != r->cols && l->rows != r->rows) {
         return NULL;
     }
-    
+
     Matrix* sum_matrix = create_matrix(l->rows, l->cols);
-    
+
     if(sum_matrix == NULL) {
         return NULL;
     }
-    
+
     int cols = sum_matrix->cols;
     int rows = sum_matrix->rows;
-    
+
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             double data = l->matrix[l->cols * i+j] + r->matrix[r->cols * i+j];
             sum_matrix->matrix[sum_matrix->cols * i+j] = data;
-            
         }
     }
-    
+
     return sum_matrix;
 }
+
 Matrix* sub(const Matrix* l, const Matrix* r){
-    if(l == NULL || r == NULL) {
+       if(l == NULL || r == NULL) {
            return NULL;
        }
-       
+
        if(l->cols != r->cols && l->rows != r->rows) {
            return NULL;
        }
-       
+
        Matrix* sub_matrix = create_matrix(l->rows, l->cols);
-       
+
        if(sub_matrix == NULL) {
            return NULL;
        }
-       
+
        int cols = sub_matrix->cols;
        int rows = sub_matrix->rows;
-       
+
        for (int i = 0; i < rows; i++) {
            for (int j = 0; j < cols; j++) {
                double data = l->matrix[l->cols * i+j] - r->matrix[r->cols * i+j];
                sub_matrix->matrix[sub_matrix->cols * i+j] = data;
-               
            }
        }
-       
+
        return sub_matrix;
+}
+
+Matrix* mul(const Matrix* l, const Matrix* r){
+    if(l == NULL || r == NULL) {
+        return NULL;
+    }
+
+    if(l->cols != r->rows && l->rows != r->cols) {
+        return NULL;
+    }
+
+    Matrix* mul_matrix = create_matrix(r->cols, l->rows);
+
+    if (mul_matrix == NULL) {
+        return NULL;
+    }
+
+    int rows = mul_matrix->rows;
+    int cols = mul_matrix->cols;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            for (size_t k = 0; k < r->rows; k++) {
+                double data = l->matrix[l->cols * i+k] * r->matrix[r->cols * k+j];
+                mul_matrix->matrix[cols * i+j] += data;
+            }
+        }
+    }
+
+    return mul_matrix;
+}
+
+int det(const Matrix* matrix, double* val){
+    if(matrix == NULL) {
+           return 0;
+       }
+       Matrix* mul_matrix = create_matrix(matrix->rows, matrix->cols);
+       if (mul_matrix == NULL) {
+           return 1;
+       }
+    val++;
+    return 2;
+}
+Matrix* adj(const Matrix* matrix){
+    if(matrix == NULL) {
+          }
+          Matrix* mul_matrix = create_matrix(matrix->rows, matrix->cols);
+          if (mul_matrix == NULL) {
+             
+          }
+    
+    return NULL;
+}
+Matrix* inv(const Matrix* matrix){
+    if(matrix == NULL) {
+          }
+          Matrix* mul_matrix = create_matrix(matrix->rows, matrix->cols);
+          if (mul_matrix == NULL) {
+          }
+    return NULL;
+}
+Matrix* create_matrix_from_file(const char* path_file){
+    if (strlen(path_file) == 0) {
+        return NULL;
+    }
+
+     return NULL;
 }
