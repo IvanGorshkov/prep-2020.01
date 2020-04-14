877bbcba5511e0beaeb21ce428251bf70c0b644d
Ivan Gorshkov <ivanagorshkov160@gmail.com>
2020-04-14 19:43:46 +0300

    fix CodeStyle

--- project/include/boundary_work.h
+++ project/include/boundary_work.h
@@ -1,8 +1,8 @@
-#ifndef PROJECT_INCLUDE_HEADER_BOUNDARY_WORK_H_
-#define PROJECT_INCLUDE_HEADER_BOUNDARY_WORK_H_
+#ifndef PROJECT_INCLUDE_BOUNDARY_WORK_H_
+#define PROJECT_INCLUDE_BOUNDARY_WORK_H_
 
 #include "models.h"
 
 int create_boundary(char *boundary, char *res_boundary, char **boundary_end, flags_t *flags);
 
-#endif  // PROJECT_INCLUDE_HEADER_BOUNDARY_WORK_H_
+#endif  // PROJECT_INCLUDE_BOUNDARY_WORK_H_
