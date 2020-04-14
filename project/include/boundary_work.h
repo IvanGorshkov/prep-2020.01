d73d1057ecde1d3720e3e7a16ca339d3e71303ff
Ivan Gorshkov <ivanagorshkov160@gmail.com>
2020-04-14 19:41:05 +0300

    fix codeStyle

--- project/include/boundary_work.h
+++ project/include/boundary_work.h
@@ -1,12 +1,8 @@
-//
-// Created by Ivan Gorshkov on 14.04.2020.
-//
-
-#ifndef PREP_2020_01_PROJECT_SRC_BOUNDARY_WORK_H_
-#define PREP_2020_01_PROJECT_SRC_BOUNDARY_WORK_H_
+#ifndef PROJECT_INCLUDE_HEADER_BOUNDARY_WORK_H_
+#define PROJECT_INCLUDE_HEADER_BOUNDARY_WORK_H_
 
 #include "models.h"
 
 int create_boundary(char *boundary, char *res_boundary, char **boundary_end, flags_t *flags);
 
-#endif //PREP_2020_01_PROJECT_SRC_BOUNDARY_WORK_H_
+#endif  // PROJECT_INCLUDE_HEADER_BOUNDARY_WORK_H_
